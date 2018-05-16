622c0db439892b41e998fe030488072fded3db77
KarlaMartinez17 <karla.martinez_@hotmail.com>
2018-05-16 12:07:32 -0500

    Modificacion a version 0.0.2, version 0.0.2 terminada

--- js/app.js
+++ js/app.js
@@ -1,24 +1,33 @@
-var showFormButton = document.getElementById('show-form-button');
-var saveButton = document.getElementById('green-button');
+/*función que esconde el boton de "añadir lista" 
+y lo reemplaza con un cuadro con un input para guardar 
+la informacion que ingrese el usuario*/
+
+var addListFormButton = document.getElementById('add-list-form-button');
 
 function hideForm(event){
     var listForm = document.getElementById('list-form');
     var showButtonContainer = document.getElementById('show-button-container');
     listForm.classList.remove('hidden');
     showButtonContainer.classList.add('hidden'); 
-  }
+}
 
-showFormButton.addEventListener('click', hideForm);
+addListFormButton.addEventListener('click', hideForm);
+
+//***************************************************************************************
+ 
+
+var saveButton = document.getElementById('save-list-button');
 
 function rescueValue(){
     
     var columnElement = document.createElement('section');
     columnElement.setAttribute('class','column');
 
-    var taskValue = document.getElementById('inputBox').value;
+    var listValue = document.getElementById('inputBox').value;
+    document.getElementById('inputBox').value = '';
     
     var textContainer = '<div id="text-container" class="add-card">'+
-                        '<p class="card-box">'+taskValue+'</p>'+
+                        '<p class="card-box">'+listValue+'</p>'+
                         '<a href="#" class="add-card-link">'+
                             'Añadir una tarjeta...'+
                         '</a>'+
@@ -33,4 +42,17 @@ function rescueValue(){
 
 saveButton.addEventListener('click', rescueValue);
 
+
+/*
+var perroBtns = document.getElementsByClassName('add-card-link');
+
+function perroFunction(event) {
+    this.parentNode.classList.add('hidden');
+    this.parentNode.nextElementSibling.classList.remove('hidden');
+}
+
+for (var i = 0; i < perroBtns.length; i++) {
+    perroBtns[i].addEventListener('click', perroFunction, false);
+}
+*/
     
